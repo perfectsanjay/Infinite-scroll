25e8b64179c9b40944d4673ccd5d98283e38539c
Sanjay Singh <sanjaysingh@Sanjays-MacBook-Pro.local>
2024-07-01 21:52:42 +0530

    infinite scroll done

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ let photolist = []
 let ready = false
 let imagesLoaded = 0
 let totalImages = 0
-const count = 10
+let count = 10
 const apiKey = 'xBB01Qk3lWCTdYJsMluZYgCJ58AmvVaayr_dexO_U7Y'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
 function imageLoaded(){
-    imageLoaded++;
+    imagesLoaded++;
     console.log('image loaded');
-    if(imageLoaded === totalImages){
+    if(imagesLoaded === totalImages){
         ready = true
         loader.hidden = true
         count = 30
@@ -28,24 +28,24 @@ function setAttribute(element,attributes){
 }
 
 function GetImage(){
-    imageLoaded = 0
-    totalImages = photoArray.length
+    imagesLoaded = 0
+    totalImages = photolist.length
 
     photolist.forEach((photo)=>{
         const item = document.createElement('a')
         // item.setAttribute('href',photo.links.html)
         // item.setAttribute('target','_blank')
         setAttribute(item,{
-            href:photo.links.html,
-            target:'_blank'
+            href: photo.links.html,
+            target: '_blank',
         })
       
         const img = document.createElement('img')
-         setAttribute(img,{
+        setAttribute(img,{
             src:photo.urls.regular,
             alt:photo.alt_description,
             title:photo.alt_description
-         })
+        })
         // img.setAttribute('src',photo.urls.regular)
         // img.setAttribute('alt',photo.alt_description)
         // img.setAttribute('title',photo.alt_description) 
